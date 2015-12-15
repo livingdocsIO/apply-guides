adc02f6d5dedce850d8a63d4fab5aa9963c7a3e3
Christian Felder (masone) <ema@rh-productions.ch>
2015-12-15 15:46:26 +0100

    get rid of validate, copy files manually

--- install.js
+++ install.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 
-var Validate = require('git-validate');
-Validate.copy('coffeelint.json', 'coffeelint.json');
-Validate.copy('.coffeelintignore', '.coffeelintignore');
-Validate.copy('.codeclimate.yml', '.codeclimate.yml');
-Validate.copy('.editorconfig', '.editorconfig');
+path = require('path');
+fs = require('fs-sync');
+
+if(fs.exists(path.resolve('package.json'))){
+  files = ['coffeelint.json', '.coffeelintignore', '.codeclimate.yml', '.editorconfig'];
+  files.forEach(function(file){
+    from = path.resolve(__dirname, file);
+    to = path.resolve(file);
+    result = fs.copy(from, to);
+    if(typeof result != 'undefined'){
+      console.log('livingdocs-apply-guides: Copied '+file);
+    } else {
+      console.log('livingdocs-apply-guides: Failed copying '+file);
+    }
+  });
+} else {
+  console.log('livingdocs-apply-guides: Not run from project root, no files copied.');
+}
